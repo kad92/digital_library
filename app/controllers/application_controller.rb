bec0ecdac3a439ba5e5d4a6e23614ec9208bd6f4
kad92 <kadiatou_diallo@hotmail.com>
2020-01-09 17:18:11 -0800

    Added devise gem, set up end user, need to fix login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+      def configure_permitted_parameters
+
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:username,:password])
+
+        devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+          user_params.permit(:username, :email, :password, :password_confirmation, :first_name, :last_name,:state,:city,:address,:phone)
+        end
+      end
 end
